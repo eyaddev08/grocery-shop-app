c5abb557a240823ae08708aaa401602d983f4be7
AyadDev <154757494+AyadDev@users.noreply.github.com>
2026-04-17 21:27:41 +0300

    feat: add location permissions and map API key configuration to Android manifest and constants

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
   abstract class AppConstants{
-        static const String mapApiKey = 'AIzaSyBw-_KXkVkRTXyaKf67Sm0puE9_eE2WwPg';
+        static const String mapApiKey = '';
 
     static const String appName = 'Grocery Shop';
      static const String slogan = 'E-Commerce Marketplace';
